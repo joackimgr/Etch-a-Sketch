8795bca6523e3e7914abef1a5e4009d67335a66d
Ioakeim <joackim.gr@gmail.com>
2025-08-03 20:19:26 +0300

    git commit -m
    git commit -m

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 function makeGrid(size) {
-    let container = document.querySelector(".container");
-    for (let i = 1; i <= size; i++) {
-        let col = document.createElement("div");
-        col.classList.add("column");
-        for (let j = 1; j <= size; j++) {
-            let row = document.createElement("div");
-            row.classList.add("row");
-            row.style.border = "1px solid black"
-            col.appendChild(row);
-        }
-        container.appendChild(col);
+  let container = document.querySelector(".container");
+  let cellSize = 600 / size;
+  container.innerHTML = "";
+  for (let i = 1; i <= size; i++) {
+    let col = document.createElement("div");
+    col.classList.add("column");
+    for (let j = 1; j <= size; j++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      row.style.border = "1px solid black";
+      row.style.width = `${cellSize}px`;
+      row.style.height = `${cellSize}px`;
+      col.appendChild(row);
     }
+    container.appendChild(col);
+  }
 }
- 
+
 function hover() {
-    let rows = document.querySelectorAll(".row");
-    rows.forEach(row => {
-        row.addEventListener("mouseover", () => row.style.backgroundColor = 'black');
-    });
+  let rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    row.addEventListener(
+      "mouseover",
+      () => (row.style.backgroundColor = "black")
+    );
+  });
+}
+
+function boxNums() {
+  let btn = document.querySelector("#btnOfNums");
+  btn.addEventListener("click", () => {
+    let num = prompt("Enter the number of squares per side (1-100)");
+    makeGrid(num);
+    hover();
+  });
 }
 
 makeGrid(16);
 hover();
+boxNums();
